30dd2090e8053af3b312c2cba5fd48d9685a7c45
Alexanderuser.name <alexander.hortua10@gmail.com>
2019-02-17 00:30:22 -0800

    adding a random init to the game

--- app.js
+++ app.js
@@ -1,14 +1,4 @@
-
-
-
-let scores, roundScore, activePlayer;
-
-scores = [0, 0];
-roundScore = 0;
-activePlayer = 0;
-gameHasfinished = 0;
-
-
+let scores, roundScore, activePlayer,gameHasfinished;
 startGame();
 
 
@@ -23,7 +13,7 @@ document.querySelector('.btn-roll').addEventListener('click', function () {
             roundScore += dice;
             document.getElementById('current-' + activePlayer).textContent = roundScore;
         } else {
-            diceDOM.style.display = 'none';
+           
             nextPlayer();
 
         }
@@ -34,40 +24,43 @@ document.querySelector('.btn-roll').addEventListener('click', function () {
 
 
 document.querySelector('.btn-hold').addEventListener('click', function () {
-    if (gameHasfinished == 0){
-    scores[activePlayer] += roundScore;
-    document.getElementById('score-' + activePlayer).textContent = scores[activePlayer];
-    scores[activePlayer] >= 10 ? winner() : nextPlayer();
-   
+    if (gameHasfinished == 0) {
+        scores[activePlayer] += roundScore;
+        document.getElementById('score-' + activePlayer).textContent = scores[activePlayer];
+        scores[activePlayer] >= 10 ? winner() : nextPlayer();
 
-}
+
+    }
 
 })
 
 
 
 document.querySelector('.btn-new').addEventListener('click', function () {
-roundScore = 0;
-document.getElementById('name-' + activePlayer).textContent = 'Player'+ activePlayer;
-document.querySelector('.player-' + activePlayer + '-panel').classList.remove('winner');
-activePlayer = 0;
-document.querySelector('.player-' + activePlayer + '-panel').classList.add('active');
-gameHasfinished = 0;
-scores = [0, 0];
+    document.getElementById('name-' + activePlayer).textContent = 'Player ' + (activePlayer+1);
+    document.querySelector('.player-' + activePlayer + '-panel').classList.remove('winner');
+    
    startGame();
-
-
 })
 
 
 function startGame() {
+    scores = [0, 0];
+    roundScore = 0;
+    activePlayer = Math.round(Math.random());
+    gameHasfinished = 0;
+    document.querySelector('.dice').style.display ='none';
+    document.querySelector('.player-0-panel').classList.remove('active');
+    document.querySelector('.player-1-panel').classList.remove('active');
+    document.querySelector('.player-' + activePlayer + '-panel').classList.add('active');
     document.getElementById('score-0').textContent = '0';
     document.getElementById('score-1').textContent = '0';
     document.getElementById('current-0').textContent = '0';
     document.getElementById('current-1').textContent = '0';
-    }
+}
 
 function nextPlayer() {
+    document.querySelector('.dice').style.display ='none';
     roundScore = 0;
     document.getElementById('current-' + activePlayer).textContent = '0';
     document.querySelector('.player-' + activePlayer + '-panel').classList.toggle('active');
@@ -77,14 +70,10 @@ function nextPlayer() {
 }
 
 function winner() {
-    gameHasfinished= 1;
+    gameHasfinished = 1;
     document.getElementById('name-' + activePlayer).textContent = 'Winner';
     document.querySelector('.dice').style.display = "none";
     document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
     document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
 }
-
-
-
-//document.querySelector('#current-'+ activePlayer).textContent = 1;
 
