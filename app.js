0931482bfbdd9bad22af4058e4e76ad607aaf9b8
Alexanderuser.name <alexander.hortua10@gmail.com>
2019-02-17 00:02:06 -0800

    adding start new game

--- app.js
+++ app.js
@@ -8,10 +8,8 @@ roundScore = 0;
 activePlayer = 0;
 gameHasfinished = 0;
 
-document.getElementById('score-0').textContent = '0';
-document.getElementById('score-1').textContent = '0';
-document.getElementById('current-0').textContent = '0';
-document.getElementById('current-1').textContent = '0';
+
+startGame();
 
 
 document.querySelector('.btn-roll').addEventListener('click', function () {
@@ -36,15 +34,38 @@ document.querySelector('.btn-roll').addEventListener('click', function () {
 
 
 document.querySelector('.btn-hold').addEventListener('click', function () {
-
+    if (gameHasfinished == 0){
     scores[activePlayer] += roundScore;
-    scores[activePlayer] >= 10 ? winner() : nextPlayer();
     document.getElementById('score-' + activePlayer).textContent = scores[activePlayer];
+    scores[activePlayer] >= 10 ? winner() : nextPlayer();
+   
 
+}
+
+})
+
+
+
+document.querySelector('.btn-new').addEventListener('click', function () {
+roundScore = 0;
+document.getElementById('name-' + activePlayer).textContent = 'Player'+ activePlayer;
+document.querySelector('.player-' + activePlayer + '-panel').classList.remove('winner');
+activePlayer = 0;
+document.querySelector('.player-' + activePlayer + '-panel').classList.add('active');
+gameHasfinished = 0;
+scores = [0, 0];
+   startGame();
 
 
 })
 
+
+function startGame() {
+    document.getElementById('score-0').textContent = '0';
+    document.getElementById('score-1').textContent = '0';
+    document.getElementById('current-0').textContent = '0';
+    document.getElementById('current-1').textContent = '0';
+    }
 
 function nextPlayer() {
     roundScore = 0;
