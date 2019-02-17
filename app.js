662da885602f02c9f3189fe1081df7104e80928d
Alexanderuser.name <alexander.hortua10@gmail.com>
2019-02-17 11:49:10 -0800

    adding a input field for the score

--- app.js
+++ app.js
@@ -1,6 +1,24 @@
-let scores, roundScore, activePlayer,gameHasfinished;
-startGame();
+/*
+YOUR 3 CHALLENGES
+Change the game to follow these rules:
 
+1. A player looses his ENTIRE score when he rolls two 6 in a row. After that, it's the next player's turn. (Hint: Always save the previous dice roll in a separate variable)
+2. Add an input field to the HTML where players can set the winning score, so that they can change the predefined score of 100. (Hint: you can read that value with the .value property in JavaScript. This is a good oportunity to use google to figure this out :)
+3. Add another dice to the game, so that there are two dices now. The player looses his current score when one of them is a 1. (Hint: you will need CSS to position the second dice, so take a look at the CSS code for the first one.)
+*/
+
+
+
+let scores, roundScore, activePlayer,gameHasfinished,maxScore;
+startGame();
+maxScore = 100;
+
+
+
+
+document.getElementById('max-score').addEventListener('change',function(){
+    maxScore = document.getElementById('max-score').value;
+    })
 
 document.querySelector('.btn-roll').addEventListener('click', function () {
     if (gameHasfinished == 0) {
@@ -27,7 +45,7 @@ document.querySelector('.btn-hold').addEventListener('click', function () {
     if (gameHasfinished == 0) {
         scores[activePlayer] += roundScore;
         document.getElementById('score-' + activePlayer).textContent = scores[activePlayer];
-        scores[activePlayer] >= 50 ? winner() : nextPlayer();
+        scores[activePlayer] >= maxScore ? winner() : nextPlayer();
 
 
     }
